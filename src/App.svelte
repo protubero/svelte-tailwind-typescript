ccbc128d021bab9473b9d7bf42a5d845fe2c3ae7
Manfred Schäfer <4920713+protubero@users.noreply.github.com>
2020-12-20 08:31:56 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import TailwindStyles from './TailwindStyles.svelte';
+
+	
+</script>
+
+<main>
+test	
+</main>
+
+<style>
+	
+	
+
+</style>
---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+const autoPreprocess = require('svelte-preprocess');
+
+module.exports = {
+  preprocess: autoPreprocess({
+    defaults: {
+      script: 'typescript',
+    },
+    postcss: {
+      plugins: [require("tailwindcss"), require("autoprefixer")],
+    }    
+  })
+};
+
